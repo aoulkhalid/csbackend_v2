dfa78442f69147c05a4ef8ffdd2da6f6e17c2b59
khalid el-aoula <120334816+aoulkhalid@users.noreply.github.com>
2025-11-26 12:19:56 +0100

    Update CORS origin for backend URL
    Replaced old backend URL with a new one in CORS configuration.

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors({
   origin: [
     "http://localhost:3000",
     "https://csclub-topaz.vercel.app",
-    "https://csbackend-v2.vercel.app"
+    "https://csbackend-33-w3v2.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
